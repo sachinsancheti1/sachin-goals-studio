28b262114d28c3008d7ebca8ce8497bc9fd30557
Sachin <sachin.sancheti@gmail.com>
2022-01-09 16:36:16 +0530

    first commit

---
+++ dashboardConfig.js
@@ -0,0 +1,11 @@
+export default {
+  widgets: [
+    { name: "project-users", layout: { height: "auto" } },
+    {
+      name: "vercel",
+      layout: {
+        width: "medium", // full width is recommended!
+      },
+    },
+  ],
+};
